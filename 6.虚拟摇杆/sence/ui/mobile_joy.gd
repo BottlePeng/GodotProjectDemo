010590385f3d8b04e9f40e53b3d108517e9f417e
BottlePeng <2329722931@qq.com>
2025-08-23 11:43:18 +0800

    虚拟摇杆

---
+++ 6.虚拟摇杆/sence/ui/mobile_joy.gd
@@ -0,0 +1,24 @@
+extends Control
+@onready var is_press = false
+@onready var out_pos = Vector2.ZERO
+@onready var out_joy = $out_joy
+@onready var in_joy = $in_joy
+
+func _input(event):
+	if event is InputEventScreenDrag or (event is InputEventScreenTouch and event.is_pressed()):
+		if event.pressed == true:
+			is_press = true
+			out_pos = get_local_mouse_position()
+		elif event.pressed == false:
+			is_press = false
+
+func _process(_delta):
+	if is_press:
+			if (get_local_mouse_position()-out_joy.position).length() >= 50:
+				out_joy.position = out_pos
+				in_joy.position = out_joy.position - 50 * (out_joy.position - get_local_mouse_position()).normalized()
+			else :
+				in_joy.position=get_local_mouse_position()
+	
+	out_joy.visible=is_press
+	in_joy.visible=is_press
